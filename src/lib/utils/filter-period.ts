5b339e00f2a9cbb2f25dac2a8332c9bf527ad36d
Olivera Leonardo <leoliveradev@gmail.com>
2026-08-25 10:40:01 -0300

    feat: refactor filterByYears function to filter by periods and include periods per year parameter

--- src/lib/utils/filter-period.ts
+++ src/lib/utils/filter-period.ts
@@ -1,3 +1,61 @@
+// export type PeriodFilterValue =
+//   | "all"
+//   | "10y"
+//   | "5y"
+//   | "3y"
+//   | "1y";
+
+// // export function filterByYears<
+// //   T extends {
+// //     anio: number;
+// //   }
+// // >(
+// //   rows: T[],
+// //   period: PeriodFilterValue
+// // ) {
+// //   if (period === "all") {
+// //     return rows;
+// //   }
+
+// //   const latestYear =
+// //     rows[rows.length - 1]?.anio ?? 0;
+
+// //   const years = {
+// //     "10y": 10,
+// //     "5y": 5,
+// //     "3y": 3,
+// //     "1y": 1,
+// //   }[period];
+
+// //   return rows.filter(
+// //     (row) =>
+// //       row.anio >=
+// //       latestYear - years
+// //   );
+// // }
+
+// export function filterByYears<
+//   T extends {
+//     anio: number;
+//   }
+// >(
+//   rows: T[],
+//   period: PeriodFilterValue
+// ) {
+//   if (period === "all") {
+//     return rows;
+//   }
+
+//   const periods = {
+//     "10y": 40,
+//     "5y": 20,
+//     "3y": 12,
+//     "1y": 4,
+//   }[period];
+
+//   return rows.slice(-periods);
+// }
+
 export type PeriodFilterValue =
   | "all"
   | "10y"
@@ -5,20 +63,14 @@ export type PeriodFilterValue =
   | "3y"
   | "1y";
 
-export function filterByYears<
-  T extends {
-    anio: number;
-  }
->(
+export function filterByPeriods<T>(
   rows: T[],
-  period: PeriodFilterValue
+  period: PeriodFilterValue,
+  periodsPerYear: number
 ) {
   if (period === "all") {
     return rows;
   }
-
-  const latestYear =
-    rows[rows.length - 1]?.anio ?? 0;
 
   const years = {
     "10y": 10,
@@ -27,9 +79,7 @@ export function filterByYears<
     "1y": 1,
   }[period];
 
-  return rows.filter(
-    (row) =>
-      row.anio >=
-      latestYear - years
+  return rows.slice(
+    -(years * periodsPerYear)
   );
 }
